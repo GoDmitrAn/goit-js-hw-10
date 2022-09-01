33cfad4639e07aa139ac9380eda9c5eaf4761201
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-09-02 02:56:20 +0300

    task begin
    add debounce, input listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import './css/styles.css';
+let debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.querySelector('#search-box');
+
+const searchParams =`fields=name,capital,population,flags,languages`;
+inputEl.addEventListener('input', debounce(userInputValue , DEBOUNCE_DELAY));
+
+function userInputValue(e) {
+    const userValue = e.target.value;
+    if(!userValue)return
+    fetchCountries(userValue)
+}
+
+function fetchCountries(countryName) {
+    fetch(`https://restcountries.com/v3.1/name/${countryName}?${searchParams}`)
+        .then(response => {
+            // console.log(response.json());
+            return response.json();
+        }).then(city => {
+            loggingCity(city)
+            }).catch(error => {
+            console.log(error)
+        });
+    function loggingCity(data) {
+        
+        if (data.length > 10) { console.log('too many matches'); }
+        else {
+            data.map(item => {
+                console.log(item.name.official);
+                const arr=item.capital
+                console.log(arr[0]);
+                 console.log('=============');
+            });
+            console.log('=================================');
+        }
+    }
+}
