1235a1346f0d3a45df6695c8dfc8bbf3e8d21051
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-09-04 13:23:43 +0300

    task complete

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,15 @@
 import { countryInfoEl, countryListEl } from "./index";
+import Notiflix from 'notiflix';
+
+Notiflix.Notify.init({
+  width: '380px',
+  position: 'center-top',
+  distance: '10px',
+  timeout: 1500
+});
 
 const searchParams = `fields=name,capital,population,flags,languages`;
+
 
 export const fetchCountries = function(countryName) {
     fetch(`https://restcountries.com/v3.1/name/${countryName}?${searchParams}`)
@@ -12,53 +21,58 @@ export const fetchCountries = function(countryName) {
         }).then(countryObj => {
                     markupCountries(countryObj);
                     }).catch(error => {
-                console.log("Country not found ",error)
+                        console.log("Country not found ", error)
+             clearArticlesContainer();
+       clearInfoContainer();
+                        Notiflix.Notify.failure('Oops, there is no country with that name');
                     });
-    // function loggingCity(data) {
-        
-    //     if (data.length > 10) { console.log('too many matches'); }
-    //     else {
-    //         data.map(item => {
-    //             console.log(item.name.official);
-    //             const arr=item.capital
-    //             console.log(arr[0]);
-    //             console.log('=============');
-    //         });
-    //         console.log('=================================');
-    //     }
-    // }
-
-   
+    
 }
 function markupCountries(data) {
-     let fullTemplate = ''
+    let fullTemplate = ''
+    let infoTemplate=''
      if (data.length > 10) {
          clearArticlesContainer();
-         console.log('too many matches');
+         clearInfoContainer();
+         
+        Notiflix.Notify.info('too many matches');
+
      } else {
           if (data.length == 1) {
               clearArticlesContainer();
-              console.log(data);
-              const oneCountry = data[0];
-              fullTemplate = `<div> <img src="${oneCountry.flags.svg}" alt="country flag" style="width:30px;height:15px">
-                                 <h1>${oneCountry.name.official}</h1></div>`
+              clearInfoContainer();
+             
+                const {flags, capital, languages,population,name} = data[0];
+              infoTemplate = `<img src="${flags.svg}" alt="country flag" class="country-flag">
+                                 <h1 class="country-name">${name.official}</h1>
+                                 <ul class="info-list"><li class="info-item"><h2>Capital:</h2><span>${capital}</span></li>
+                                                            <li class="info-item"><h2>Population:</h2><span>${population}</span></li>
+                                                            <li class="info-item"><h2>Languages:</h2><span>${Object.values(languages)}</span></li></ul>`
      } else {
          clearArticlesContainer();
+         clearInfoContainer();
         
-        data.map(item => {
-            console.log(item);
-            const template = `<li><span style="margin-right:20px;display:inlone-block">
-            <img src="${item.flags.svg}" alt="country flag" style="width:30px;height:15px"></span><p style="display: inline-block">${item.name.official}</p></li>`;
-            fullTemplate += template;
+        fullTemplate =   data.map(item => {
+                
+                  return `<li class="country-list__item">
+            <img src="${item.flags.svg}" alt="country flag"><p>${item.name.official}</p></li>`;
+                  
         
-        })
+              }).join('');
           
         }
      }
     
     
     countryListEl.insertAdjacentHTML("afterbegin", fullTemplate);
-    }
+    countryInfoEl.insertAdjacentHTML("afterbegin", infoTemplate);
+
+    
+}
+    
     function clearArticlesContainer() {
-    countryListEl.innerHTML = '555555';
+        countryListEl.innerHTML = '';
+}
+ function clearInfoContainer() {
+        countryInfoEl.innerHTML = '';
 }
--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ const countryListEl = document.querySelector('.country-list')
 const countryInfoEl = document.querySelector('.country-info')
 export {countryListEl,countryInfoEl}
 import './css/styles.css';
+import './css/user-style.css'
 import { fetchCountries } from './fetchCountries'
 let debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('#search-box');
 
-countryListEl.style.listStyle="none"
+
 inputEl.addEventListener('input', debounce(userInputValue , DEBOUNCE_DELAY));
 
 function userInputValue(e) {
     const userValue = e.target.value.trim();
-    if(!userValue)return
+    if (!userValue) {
+        countryListEl.innerHTML = '';
+        countryInfoEl.innerHTML = '';
+        return
+    }
     fetchCountries(userValue)
 }
 
